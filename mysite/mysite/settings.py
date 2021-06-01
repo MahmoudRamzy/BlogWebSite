ac92f4ee2f794eac115b5d27142e69f577ebed90
mahmoudramzy <eng_mru@hotmail.com>
2021-06-02 01:52:39 +0200

    Start - Retrieving posts by similarity

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'mysite.urls'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 TEMPLATES = [
     {
